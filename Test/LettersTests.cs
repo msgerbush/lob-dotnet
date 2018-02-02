1f03e07eb158c001c63f0a588395ab9d1a4e7ea0
Michael Gerbush <mgerbush@embryooptions.com>
2018-02-02 13:05:52 -0500

    Add error handling for non successful API requests
    In the event of an error status code from the API, throw an
    exception with the error details, instead of trying to parse
    default object.
    
    - Add custom LobException class to contain ApiResponse with status
    code and error object properties
    - Add LobError response object to deserialize error JSON into object
    - Add simple test case for handling error response when Letter to
    address is empty
    - Add Netwonsoft.Json package to Test project to fix conflicting
    package error with Lob library

--- Test/LettersTests.cs
+++ Test/LettersTests.cs
@@ -1,4 +1,7 @@
-﻿using Lob;
+﻿using System.Net;
+using System.Threading.Tasks;
+using Lob;
+using Lob.Http;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace LobTests
@@ -6,12 +9,12 @@ namespace LobTests
     [TestClass]
     public class LettersTests
     {
+        private static readonly Credentials TestCredentials = new Credentials("test_fd34e1b5ea86a597ec89f7f2e46940c874d");
+        private static readonly LobClient LobClient = new LobClient("test-app", new CredentialStore(TestCredentials));
+
         [TestMethod]
         public void TestCreate()
         {
-            Credentials testCredentials = new Credentials("test_fd34e1b5ea86a597ec89f7f2e46940c874d");
-            var lobClient = new LobClient("test-app", new CredentialStore(testCredentials));
-
             object fromAddress = new {
                 name = "Test Sender",
                 address_line1 = "185 Berry st",
@@ -37,7 +40,42 @@ namespace LobTests
                 file: "url"
             );
 
-            lobClient.Letter.Create(letter);
+            LobClient.Letter.Create(letter);
+        }
+
+        [TestMethod]
+        public async Task TestEmptyToAddress()
+        {
+            object fromAddress = new
+            {
+                name = "Test Sender",
+                address_line1 = "185 Berry st",
+                address_city = "San Francisco",
+                address_state = "CA",
+                address_zip = "94107",
+                address_country = "US"
+            };
+
+            object toAddress = new
+            {
+                name = "Test Recipient",
+                address_line1 = "",
+                address_city = "San Francisco",
+                address_state = "CA",
+                address_zip = "94107",
+                address_country = "US"
+            };
+
+            NewLetter letter = new NewLetter(
+                description: "test letter",
+                to: toAddress,
+                from: fromAddress,
+                file: "<!doctype html><meta charset=utf-8><body>Test</body></html>"
+            );
+
+            var exception = await Assert.ThrowsExceptionAsync<LobException>(() => LobClient.Letter.Create(letter));
+            Assert.AreEqual(exception.HttpStatusCode, (HttpStatusCode) 422);
+            StringAssert.Contains(exception.Message, "to.address_line1 is required");
         }
     }
 }
